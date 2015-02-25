e32032f22c63091c562c66392c3ebe14a7e22cf5
greg <gp_github@swimr.com>
2015-02-25 15:22:40 -0800

    Initial Commit

---
+++ UavServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UavServer
+//
+//  Created by gp on 2/25/15.
+//  Copyright (c) 2015 swimr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
